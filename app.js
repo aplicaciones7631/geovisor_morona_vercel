53ac710341d602e73b0cab3884e0cbe1011e7a39
aplicaciones7631 <aplicaciones7631@users.noreply.github.com>
2026-07-25 16:19:30 -0500

    Renombra boton PDF a Impresion de mapa

--- app.js
+++ app.js
@@ -995,7 +995,7 @@ async function generatePDF() {
         <line x1="16" y1="13" x2="8" y2="13"/>
         <line x1="16" y1="17" x2="8" y2="17"/>
       </svg>
-      PDF`;
+      Impresión de mapa`;
   }
 }
 
